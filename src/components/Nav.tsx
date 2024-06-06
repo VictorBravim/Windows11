d214319b1782e04cba1c1ed8d4ddb001d36946d7
Victor Bravim <victorrafaelbravim@gmail.com>
2024-06-06 17:58:24 -0300

    Mudança

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FaWifi, FaVolumeUp } from 'react-icons/fa';
-import { FcGoogle } from "react-icons/fc";
-import { SiVisualstudiocode } from "react-icons/si";
+import { FcGoogle } from 'react-icons/fc';
+import { SiVisualstudiocode } from 'react-icons/si';
 
 const Nav: React.FC = () => {
   const currentDate = new Date().toLocaleString('pt-BR', {
@@ -12,10 +12,18 @@ const Nav: React.FC = () => {
   return (
     <div className="w-full fixed bottom-0 bg-gray-800 text-white flex justify-between items-center px-4 py-2">
       <div className="flex items-center space-x-2 pr-20">
+        <button className="bg-gray-900 rounded-full p-1 focus:outline-none focus:ring-2 focus:ring-blue-500">
+          <FcGoogle className="text-2xl" />
+        </button>
+        <button className="bg-gray-900 rounded-full p-1 focus:outline-none focus:ring-2 focus:ring-blue-500">
+          <SiVisualstudiocode className="text-2xl" />
+        </button>
       </div>
       <div className="flex-1 flex justify-center space-x-4">
-        <FcGoogle className="text-2xl" />
-        <SiVisualstudiocode className="text-2xl" />
+        <button className="bg-gray-900 rounded-full p-1 focus:outline-none focus:ring-2 focus:ring-blue-500">
+          {/* Adicione o ícone do menu Iniciar do Windows 11 aqui */}
+        </button>
+        {/* Adicione outros itens da barra de tarefas do Windows 11 conforme necessário */}
       </div>
       <div className="flex items-center space-x-2">
         <span>{currentDate}</span>
