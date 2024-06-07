2b7cd2aebfcff1c87c695a093231e8566f1f6647
Victor Bravim <victorrafaelbravim@gmail.com>
2024-06-07 15:31:39 -0300

    Mudança

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { IoWifi, IoVolumeHighOutline } from "react-icons/io5";
 import { GoBell } from "react-icons/go";
+import { IoIosArrowUp } from "react-icons/io";
 import Image from 'next/image';
 import windows from '@/assets/menu.png';
 import store from '@/assets/store.png';
@@ -15,36 +16,37 @@ const Nav: React.FC = () => {
     const currentTime = currentDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 
     return (
-        <div className="w-full fixed bottom-0 bg-gray-800 text-white flex justify-between items-center px-4 py-2">
-            <div className="flex items-center space-x-2 pr-20">
+        <div className="w-full fixed bottom-0 bg-gray-800 text-white flex justify-between items-center px-4 py-1">
+            <div className="flex items-center space-x-2 pr-24">
                 <Image src={clima} alt="Windows Menu" width={100} height={32} />
             </div>
-            <div className="flex-1 flex justify-center space-x-4">
-                <button className="bg-transparent hover:bg-gray-900 rounded-md p-1 focus:outline-none focus:ring-2 focus:ring-blue-500">
-                    <Image src={windows} alt="Windows Menu" width={32} height={32} />
+            <div className="flex-1 flex justify-center space-x-2">
+                <button className="bg-transparent hover:bg-gray-700 rounded-md p-2">
+                    <Image src={windows} alt="Windows Menu" width={28} height={32} />
                 </button>
-                <button className="bg-transparent hover:bg-gray-900 rounded-md p-1 focus:outline-none focus:ring-2 focus:ring-blue-500">
-                    <Image src={store} alt="Windows Menu" width={32} height={32} />
+                <button className="bg-transparent hover:bg-gray-700 rounded-md p-2">
+                    <Image src={store} alt="Windows Menu" width={28} height={32} />
                 </button>
-                <button className="bg-transparent hover:bg-gray-900 rounded-md p-1 focus:outline-none focus:ring-2 focus:ring-blue-500">
-                    <Image src={archive} alt="Windows Menu" width={32} height={32} />
+                <button className="bg-transparent hover:bg-gray-700 rounded-md p-2">
+                    <Image src={archive} alt="Windows Menu" width={28} height={32} />
                 </button>
-                <button className="bg-transparent hover:bg-gray-900 rounded-md p-1 focus:outline-none focus:ring-2 focus:ring-blue-500">
-                    <Image src={google} alt="Windows Menu" width={32} height={32} />
+                <button className="bg-transparent hover:bg-gray-700 rounded-md p-2">
+                    <Image src={google} alt="Windows Menu" width={28} height={32} />
                 </button>
-                <button className="bg-transparent hover:bg-gray-900 rounded-md p-1 focus:outline-none focus:ring-2 focus:ring-blue-500">
-                    <Image src={vscode} alt="Windows Menu" width={32} height={32} />
+                <button className="bg-transparent hover:bg-gray-700 rounded-md p-2">
+                    <Image src={vscode} alt="Windows Menu" width={28} height={32} />
                 </button>
             </div>
-            <div className="flex items-center space-x-2">
+            <div className="flex items-center text-xl space-x-2 pr-2">
+            <IoIosArrowUp className='pr-2 text-2xl' />
                 <IoWifi />
                 <IoVolumeHighOutline />
             </div>
-            <div className="flex items-center space-x-2 flex-col">
+            <div className="flex text-sm items-end space-x-2 flex-col pr-2">
                 <span>{currentTime}</span>
                 <span>{formattedDate}</span>
             </div>
-            <div className="flex items-center space-x-2">
+            <div className="flex items-center text-xl space-x-2">
                 <GoBell />
             </div>
         </div>
