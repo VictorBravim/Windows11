41566f062ee6e992bf8b84bf04d9d44d996b2355
Victor Bravim <victorrafaelbravim@gmail.com>
2024-06-07 13:32:21 -0300

    Mudança

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import { FaWifi, FaVolumeUp} from 'react-icons/fa';
-import { FcGoogle } from 'react-icons/fc';
-import { SiVisualstudiocode } from 'react-icons/si';
+import { FaWifi, FaVolumeUp } from 'react-icons/fa';
+import Image from 'next/image';
+import windows from '@/assets/menu.png';
+import store from '@/assets/store.png';
+import archive from '@/assets/archive.png';
+import google from '@/assets/google.png';
+import vscode from '@/assets/vscode.png';
 
 const Nav: React.FC = () => {
     const currentDate = new Date().toLocaleString('pt-BR', {
@@ -15,14 +19,24 @@ const Nav: React.FC = () => {
         <div className="w-full fixed bottom-0 bg-gray-800 text-white flex justify-between items-center px-4 py-2">
             <div className="flex items-center space-x-2 pr-20">
                 <button className="bg-gray-900 rounded-full p-1 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                    <Image src={windows} alt="Windows Menu" width={32} height={32} />
                 </button>
             </div>
             <div className="flex-1 flex justify-center space-x-4">
-                <button className="bg-gray-900 rounded-full p-1 focus:outline-none focus:ring-2 focus:ring-blue-500">
-                    <FcGoogle className="text-2xl" />
+                <button className="bg-transparent hover:bg-gray-900 rounded-md p-1 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                    <Image src={windows} alt="Windows Menu" width={32} height={32} />
                 </button>
-                <button className="bg-gray-900 rounded-full p-1 focus:outline-none focus:ring-2 focus:ring-blue-500">
-                    <SiVisualstudiocode className="text-2xl" />
+                <button className="bg-transparent hover:bg-gray-900 rounded-md p-1 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                    <Image src={store} alt="Windows Menu" width={32} height={32} />
+                </button>
+                <button className="bg-transparent hover:bg-gray-900 rounded-md p-1 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                    <Image src={archive} alt="Windows Menu" width={32} height={32} />
+                </button>
+                <button className="bg-transparent hover:bg-gray-900 rounded-md p-1 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                    <Image src={google} alt="Windows Menu" width={32} height={32} />
+                </button>
+                <button className="bg-transparent hover:bg-gray-900 rounded-md p-1 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                    <Image src={vscode} alt="Windows Menu" width={32} height={32} />
                 </button>
             </div>
             <div className="flex items-center space-x-2">
